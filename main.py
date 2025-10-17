407c84e321646d31d09980addb1c4d0fadd13404
Rohit-specs <rohit5592006@gmail.com>
2025-10-17 15:30:12 +0530

    deleted commented lines

--- main.py
+++ main.py
@@ -1,14 +1,3 @@
 from authentication import authentication_menu
 authentication_menu.menu()
 
-
-# # from colorama import init,Fore,Back,Style
-# # print(Fore.RED+"hello")
-# # print(Fore.GREEN+"hello")
-# # print(Fore.YELLOW+"hello")
-# # print(Back.BLUE+"hello")
-# # print(Style.BRIGHT+"hello")
-# # print(Style.DIM+"hello")
-# # print(Style.NORMAL+"hello")
-# # print(Style.RESET_ALL,end="")
-# # print("hello")
