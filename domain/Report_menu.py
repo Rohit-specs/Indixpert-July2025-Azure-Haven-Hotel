e14748a47722e309348af467e4bf17e33ff7627e
Rohit-specs <rohit5592006@gmail.com>
2025-10-26 16:06:24 +0530

    assigining 1 object to one class

--- domain/Report_menu.py
+++ domain/Report_menu.py
@@ -1,5 +1,6 @@
 from report import Report_data
 def report_menu():
+    obj=Report_data()
     while True:
         print("\n-----------------Report_Menu-----------------")
         print("1.  Total no. Of Booking")
@@ -20,43 +21,33 @@ def report_menu():
         #     obj=domain.log(error,__name__)
         #     continue
         if choice==1:
-            obj=Report_data()
             obj.no_of_booking()
         
         elif choice==2:
-            obj=Report_data()
             obj.current_number_of_booking()
         
         elif choice==3:
-            obj=Report_data()
             obj.total_revenue()
 
         elif choice==4:
-            obj=Report_data()
             obj.current_month_total_revenue()
 
         elif choice==5:
-            obj=Report_data()
             obj.average_booking_duration()
 
         elif choice==6:
-            obj=Report_data()
             obj.current_month_average_booking_duration()
 
         elif choice==7:
-            obj=Report_data()
             obj.most_ordered_dish()
 
         elif choice==8:
-            obj=Report_data()
             obj.current_month_most_ordered_dish()
         
         elif choice==9:
-            obj=Report_data()
             obj.most_ordered_dish()
 
         elif choice==10:
-            obj=Report_data()
             obj.current_month_payment_method_used()
 
         elif choice==11:
