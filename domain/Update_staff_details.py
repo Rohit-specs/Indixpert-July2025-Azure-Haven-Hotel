49342e4381af54ae967b9203f55f7f6dfcf43766
Rohit-specs <rohit5592006@gmail.com>
2025-10-30 15:03:09 +0530

    update colours

--- domain/Update_staff_details.py
+++ domain/Update_staff_details.py
@@ -4,6 +4,7 @@ from validation import valid_password,valid_email,valid_name
 import getpass
 import domain
 from colorama import init,Back,Fore,Style
+init(autoreset=True)
 class update_staff:
     attempt=4
     def __init__(self,path):
@@ -17,13 +18,13 @@ class update_staff:
                     file.write(data)
             except Exception as error:
                 log_obj=domain.log(error,__name__)
-                print("Error while opening the file")
+                print(Fore.RED+"Error while opening the file")
         try:        
             with open(self.path,'r') as data:
                 self.staff_data=json.loads(data.read())
         except Exception as error:
             log_obj=domain.log(error,__name__)
-            print("Error while loading the file") 
+            print(Fore.RED+"Error while loading the file") 
         # self.staff_data=domain.file_reader(self.path,__name__)
 
     def save(self):
@@ -33,7 +34,7 @@ class update_staff:
                 data.write(modified_data)      
         except Exception as error:
             log_obj=domain.log(error,__name__)
-            print("Error occurring while Updating staff data")
+            print(Fore.RED+"Error occurring while Updating staff data")
             
             
     def remove_staff(self):
@@ -44,7 +45,7 @@ class update_staff:
             while True:
                 self.role=input("Enter you role: ").lower()
                 if self.role!="staff" and self.role!="admin":
-                    print("You have entered wrong role\nchoose between (staff/admin)")
+                    print(Fore.RED"You have entered wrong role\nchoose between (staff/admin)")
                     continue
                 break        
             for user in self.staff_data:
@@ -53,13 +54,13 @@ class update_staff:
                         if user.get("role")==self.role:
                             self.staff_data.remove(user)
                             self.save()
-                            print("Staff removed successfully")
+                            print(Fore.GREEN+"Staff removed successfully")
                         else:
-                            print("Role mismatch")
+                            print(Fore.RED+"Role mismatch")
                     else:
-                        print("email not found in Record")
+                        print(Fore.RED+"email not found in Record")
         except Exception as error:
-            print("Error occurring while removing a staff")
+            print(Fore.RED+"Error occurring while removing a staff")
             log_obj=domain.log(error,__name__)
             
                 
@@ -74,29 +75,29 @@ class update_staff:
                 while True:
                     self.role=input("Enter you role: ").lower()
                     if self.role!="staff" and self.role!="admin":
-                        print("You have entered wrong role\nchoose between (staff/admin)")
+                        print(Fore.RED+"You have entered wrong role\nchoose between (staff/admin)\n")
                         continue
                     break       
                 if password==confirm_password:
                     new_staff={"name":name,"email":email,"password":password,"role":self.role}
                     self.staff_data.append(new_staff)
                     self.save()
-                    print("Staff added successfully...")
+                    print(Fore.GREEN+"Staff added successfully...")
                     return
                     
                 else:
                     attempt-=1
                     if attempt==0:
-                        print("No attempt left now")
+                        print(Fore.RED+"No attempt left now")
                         choice=input("Do you want to create a password again(yes/no): ")
                         if choice.lower()=="yes":
                             break
                         elif choice.lower()=="no":
                             domain.admin_menu()
                         else:
-                            print(choice,"is not a valid option")                            
+                            print(Fore.GREEN+str(choice)+"is not a valid option")                            
                     else:        
-                        print("you have now",attempt,"attempt left.")
+                        print(Fore.YELLOW+Style.DIM+"you have now"+str(attempt)+"attempt left.")
 
     def staff_details(self):
         i=1
