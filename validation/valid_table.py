f7b7d316287c7ebd2f136d4f9f08f645a063d9e3
Rohit-specs <rohit5592006@gmail.com>
2025-10-30 21:31:37 +0530

    added colour in print statement

--- validation/valid_table.py
+++ validation/valid_table.py
@@ -1,3 +1,5 @@
+from colorama import init,Fore
+init(autoreset=True)
 import domain
 def valid_table(a="table number you want to book"):
     while True:
@@ -6,13 +8,13 @@ def valid_table(a="table number you want to book"):
             table_no=int(input())
         except Exception as error:
             obj=domain.log(error,__name__)
-            print("invalid input Please try again")
+            print(Fore.RED+"invalid input Please try again\n")
             continue
         if table_no>50:
-            print("Hotel have only 50 tables")
+            print(Fore.RED+"Hotel have only 50 tables\n")
             continue
         elif table_no<=0:
-            print("Invalid number")
+            print(Fore.RED+"Invalid number\n")
             continue
         else:
             return table_no
